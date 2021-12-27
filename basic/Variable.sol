57a5a6421b84fa4cfe5ef2cf1ad9613c9bef34ae
duc.tbmd <duc.tbmd@gmail.com>
2021-12-28 01:33:22 +0700

    string, address

--- basic/Variable.sol
+++ basic/Variable.sol
@@ -6,4 +6,36 @@ contract WorkingWithVariables {
     function setMyUnit(uint _myUint) public {
         myUnit = _myUint;
     }
+
+    bool public myBool;
+
+     function setMyBool(bool _myBool) public {
+         myBool = _myBool;
+     }
+
+    uint8 public myUint8;
+
+    function incrementUint() public {
+        myUint8++;
+    }
+
+    function decrementUint() public {
+        myUint8--;
+    }
+
+    address public myAddress;
+
+    function setAddress(address _address) public {
+        myAddress = _address;
+    }
+
+    function getBalanceOfAddress() public view returns (uint) {
+        return myAddress.balance;
+    }
+
+    string public myString = 'Hello world';
+    
+    function setMyString(string memory _myString) public {
+        myString = _myString;
+    }
 }
